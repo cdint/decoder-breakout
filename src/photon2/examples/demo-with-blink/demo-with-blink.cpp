da9399a10c3719eb0b7a46d2d6575c18ef0d10b2
Steve Traugott <stevegt@t7a.org>
2025-04-03 11:37:04 -0700

    Add Makefile and enhance blink code readability in photon2 directory
    Add Makefile to photon2 with build, flash, and monitor targets
    
    summary of diff --git  a/src/photon2/Makefile b/src/photon2/Makefile
    
    - Add Makefile to the photon2 directory under src
    - Set the file mode to 100644
    - Include target 'all' that depends on 'ship' and 'monitor'
    - Define 'ship' target to depend on 'build' and 'flash'
    - Create 'build' target to compile with 'particle compile p2'
    - Create 'flash' target to flash locally and delay for 5 seconds
    - Create 'monitor' target with 'particle serial monitor --follow'
    - Note the absence of a newline at the end of the file
    
    Clean up code, add blink control, reformat, improve readability
    
    summary of diff --git  a/src/photon2/examples/demo-with-blink/demo-with-blink.cpp b/src/photon2/examples/demo-with-blink/demo-with-blink.cpp
    
    - Remove unnecessary trailing spaces
    - Introduce the `blinkMoves` boolean flag to control LED blinking behavior
    - Wrap LED control code with a conditional check against `blinkMoves`
    - Reformat and align comments for consistency
    - Reformat `setup()` and `loop()` functions for improved readability
    - Add conditional LED flashing for identical count values within the `loop()`
    - Comment out `Particle.process()` since it's not needed in `SYSTEM_THREAD` mode
    - Reorganize delay calls with a focus on `SYSTEM_THREAD` mode compatibility

--- src/photon2/examples/demo-with-blink/demo-with-blink.cpp
+++ src/photon2/examples/demo-with-blink/demo-with-blink.cpp
@@ -2,7 +2,7 @@
 #include <decoder-breakout.h>
 
 // enable system background thread
-SYSTEM_THREAD(ENABLED); 
+SYSTEM_THREAD(ENABLED);
 
 #define POLL_INTERVAL 200
 // unsigned long nextPollMillis = 0;
@@ -13,17 +13,25 @@ char msgBuff[128];
 // ADDR_JUMPERS is the address set by the jumpers on the board
 #define ADDR_JUMPERS 0b111
 
-#define CNTR_SIZE 4   // number of bytes to configure counters
+#define CNTR_SIZE 4 // number of bytes to configure counters
 
 DecoderBreakout chip(ADDR_JUMPERS, CNTR_SIZE);
 
-void setup() {
+// set blinkMoves true to blink Photon 2 LED different colors
+// when encoder moves.  Setting this to false will leave LED
+// under control of Particle OS.
+bool blinkMoves = false;
 
-    // Take control of the RGB LED
-    RGB.control(true);
+void setup()
+{
 
-    // Setup I2C Buss Master  
-    Wire.begin();    // Set i2c master mode
+    if (blinkMoves) {
+        // Take control of the RGB LED
+        RGB.control(true);
+    }
+
+    // Setup I2C Buss Master
+    Wire.begin(); // Set i2c master mode
 
     // Set Bus Speed to Fast Mode 400KHz
     // unsigned long mstrClock = 400000l;
@@ -31,18 +39,19 @@ void setup() {
     // Particle might instead use Wire.setSpeed(CLOCK_SPEED_400KHZ);
 
     // Setup I2c Timeouts (default)
-    // Wire.setWireTimeout();    
+    // Wire.setWireTimeout();
 
     // Setup Serial Monitor
-    Serial.begin(9600);         // Setup serial monitor baud rate
-    delay(5000);                // Wait for Serial Monitor to start
+    Serial.begin(9600); // Setup serial monitor baud rate
+    delay(5000);        // Wait for Serial Monitor to start
 
     // Setup LS7866 registers
     Serial.print("Setting up counter chip.\r\n");
     bool ok = chip.ChipSetup();
-    if (!ok){
-      Serial.print("Error setting up chip.\r\n");
-      return;
+    if (!ok)
+    {
+        Serial.print("Error setting up chip.\r\n");
+        return;
     }
     byte chipAddress = chip.GetChipAddress();
     sprintf(msgBuff, "Using library version %s\r\n", DECODER_BREAKOUT_LIBRARY_VERSION);
@@ -52,48 +61,61 @@ void setup() {
     sprintf(msgBuff, "Setup Counter %d at Addr:%02x MCR0:%02x MCR1:%02x\r\n", ADDR_JUMPERS, chipAddress, MCR0, MCR1);
     Serial.print(msgBuff);
 
-
     Serial.print("Setup complete.\r\n");
     Serial.print("Starting Device Polling.\r\n");
 
     // nextPollMillis = millis() + POLL_INTERVAL;
- 
 }
 
+void loop()
+{
 
-void loop() {
-  
-  unsigned long count = 0;
-  bool verbose = true; // true to always print, false to only print when count changes
-  
-  // Check for polling interval
-  //  if (millis() > nextPollMillis){
-  //  nextPollMillis = millis() + POLL_INTERVAL;   // save next polling time stamp
-  
-    count = chip.CounterPoll(); 
-    if (count > prevCount){
-      // flash Photon 2 LED green
-      RGB.color(0, 255, 0);
-      verbose = true;
+    unsigned long count = 0;
+    bool verbose = true; // true to always print, false to only print when count changes
+
+    // Check for polling interval
+    //  if (millis() > nextPollMillis){
+    //  nextPollMillis = millis() + POLL_INTERVAL;   // save next polling time stamp
+
+    count = chip.CounterPoll();
+
+    if (blinkMoves)
+    {
+        if (count > prevCount)
+        {
+            // flash Photon 2 LED green
+            RGB.color(0, 255, 0);
+        }
+        if (count < prevCount)
+        {
+            // flash Photon 2 LED red
+            RGB.color(255, 0, 0);
+        }
+        if (count == prevCount)
+        {
+            // flash Photon 2 LED white
+            RGB.color(255, 255, 255);
+        }
     }
-    if (count < prevCount){
-      // flash Photon 2 LED red
-      RGB.color(255, 0, 0);
-      verbose = true;
+
+    if (verbose)
+    {
+        // sprintf(msgBuff, "Counter %d: %08d millis %d nextPollMillis %d\n", ADDR_JUMPERS, count, millis(), nextPollMillis);
+        sprintf(msgBuff, "Counter %d: %08d\n", ADDR_JUMPERS, count);
+        Serial.print(msgBuff);
     }
-    if (count == prevCount){
-      // flash Photon 2 LED white
-      RGB.color(255, 255, 255);
+
+    delay(POLL_INTERVAL / 2);
+
+    if (blinkMoves)
+    {
+        // turn off Photon 2 LED
+        RGB.color(0, 0, 0);
     }
-    if (verbose){
-      // sprintf(msgBuff, "Counter %d: %08d millis %d nextPollMillis %d\n", ADDR_JUMPERS, count, millis(), nextPollMillis);
-      sprintf(msgBuff, "Counter %d: %08d\n", ADDR_JUMPERS, count);
-      Serial.print(msgBuff);
-    }
-    delay(POLL_INTERVAL/2);
-    RGB.color(0, 0, 0);
+
     prevCount = count;
 
-    Particle.process();  // Maintain cloud connection
-    delay(POLL_INTERVAL/2);
+    // Particle.process();  // Maintain cloud connection (not needed in SYSTEM_THREAD mode)
+    // Calling delay() will yield to the system thread when in SYSTEM_THREAD mode
+    delay(POLL_INTERVAL / 2);
 }
